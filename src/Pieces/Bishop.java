4c9e33330ce6aeb60d14f88cab0430371d686bbf
Johnny Tanner <jmt177@truman.edu>
2015-07-08 12:36:17 -0500

    Finish basic pawn movement, start bishop movement

--- src/Pieces/Bishop.java
+++ src/Pieces/Bishop.java
@@ -5,6 +5,7 @@
  */
 package Pieces;
 
+import ChessBoard.Board;
 import Movement.PieceMovement;
 import chessgame.ChessTeam;
 
@@ -17,20 +18,72 @@ public class Bishop extends Piece {
     public Bishop(char team, int[] location, ChessTeam myTeam) {
         super(team, location, myTeam);
     }
-
+    
+    Board board = Board.getInstance();
+    
     @Override
     public boolean legalMove(Piece piece, int[] toMove) {
-        return true;
+        // if it doesn't move return false, otherwise might take their turn 
+        // doing nothing.
+        // The bishop moves equal amounts of rows and columns, if the difference
+        // is not the same, not a valid move
+        if((piece.getColumn() == toMove[0] && piece.getRow() == toMove[1]) ||
+           Math.abs(piece.getColumn() - toMove[0]) != Math.abs(piece.getRow() - toMove[1])){
+            return false;
+        }
+   
+       if(inBoard(toMove) && (board.onSquare(toMove[0], toMove[1]) == null || noAlly(piece, toMove))){
+           return true;
+       }
+        return false;
     }
 
     @Override
     public boolean inBoard(int[] toMove) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        return(toMove[0] <= board.getLength() && 
+               toMove[1] <= board.getWidth());
+           
     }
 
     @Override
     public boolean noAlly(Piece piece, int[] toMove) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // if the location to move is in the board and not occupied by an allied piece, go for it
+        if(inBoard(toMove) && board.onSquare(toMove[0], toMove[1]).getTeam() != piece.getTeam()){
+            // if the column is correct, moving down the row
+            if(piece.getColumn() == toMove[0]){
+                if(piece.getRow() > toMove[1]){
+                    return privNoAlly(piece, toMove, new int[]{-1, 0});
+                } else {
+                    return privNoAlly(piece, toMove, new int[]{1, 0});                 
+                }
+            // if the row is correct, moving down the column
+            } else if(piece.getRow() == toMove[1]) {
+                if(piece.getColumn() > toMove[0]){
+                    return privNoAlly(piece, toMove, new int[]{0, -1});
+                } else {
+                    return privNoAlly(piece, toMove, new int[]{0, 1});                 
+                }                
+            }
+        }
+        return false;
     }
-    
+    // need to encapsulate logic for all these god damn if statements DRY
+    // rowColumn is the row or column int, target is toMove int,
+    // target holds a 1/-1 and a 0, representing the direction to move
+    // need to be able to add the direction to itself, hence tRow tCol
+    // currently incorrect, does not account for ending on target while
+    // target is occupied with an enemy piece
+    private boolean privNoAlly(Piece piece, int[] toMove, int[] dir){
+        int tRow = dir[1];
+        int tCol = dir[0];
+        while(piece.getRow() + tRow != toMove[1] && piece.getColumn() + tCol != toMove[0]){
+            if(board.onSquare(piece.getColumn() + tRow, piece.getRow() + tCol) != null){
+                return false;
+            }
+            tRow += dir[1];
+            tCol += dir[0];
+        }
+        return true;
+    }
 }
--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -13,9 +13,10 @@ import chessgame.ChessTeam;
  *
  * NEED TO IMPLEMENT:
  * En Passant
+ * Promotion
+ * NEED TO TEST:
  * First move can be one or two squares
  * Attack diagonally only
- * Promotion
  */
 public class Pawn extends Piece {
     private int dir;
@@ -48,13 +49,9 @@ public class Pawn extends Piece {
             if(piece.getColumn() == toMove[0] && board.onSquare(toMove[0], toMove[1]) == null){
                 // If the piece has not moved, can move forward 2 OR
                 // movement distance is 1
-                if((starting[1] == piece.getRow() && Math.abs(toMove[1] - piece.getRow()) == 2)
-                   || Math.abs(toMove[1] - piece.getRow()) == 1){
-                    return true;
-                } 
                 // Returns true here if correct direction and the distance to move is 1
-                return false;
-                
+                return (starting[1] == piece.getRow() && Math.abs(toMove[1] - piece.getRow()) == 2)
+                        || Math.abs(toMove[1] - piece.getRow()) == 1;
             // Diagonal movement, only for attacking. toMove already passed inBoard, no need to worry about that
             } else {
                 // only allowed to move 1
@@ -66,14 +63,14 @@ public class Pawn extends Piece {
                    board.onSquare(toMove[0], toMove[1]).getTeam() == this.getTeam()){
                     return false;
                 }
-
+                // Only may attack one square away
                 if(Math.abs(toMove[0] - piece.getColumn()) == 1 && 
                    Math.abs(toMove[1] - piece.getRow()) == 1){
                     return true;
                 }
             }
         }
-        // if we get here, most likely don't want to let it move
+        // Not in board
         return false;
     }
 
@@ -85,7 +82,9 @@ public class Pawn extends Piece {
 
     @Override
     public boolean noAlly(Piece piece, int[] toMove) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // The unique movment of pawns makes this unnecessary for now
+        // will likely need for En Passant
+        return board.onSquare(toMove[0], toMove[1]) == null;
     }
 
 }
